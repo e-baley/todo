0e12d5dabf618fecebe1b3e80b08c4093138fe26
ebaley <florian.hacquebart@gmail.com>
2019-12-15 18:38:24 +0100

    we can write todo to the file

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ const char* filename = ".todolist";
 enum CMD_TYPE {VIEW, INSERT, COMPLETE};
 
 int define_command(int *argc, char** argv);
+int insert_todo(FILE* f, char** todo);
 
 int define_command(int *argc, char** argv) {
     if(*argc <= 1)
@@ -16,15 +17,40 @@ int define_command(int *argc, char** argv) {
     return INSERT;
 }
 
+int insert_todo(FILE* f, char** todo) {
+    if(f == NULL) return -1;
+
+    while(1) {
+        fprintf(f, "%s", *(todo));
+        todo = todo+1;
+        if(*(todo) == NULL) break;
+        fprintf(f, " ");
+    }
+    fprintf(f, "\n");
+    
+    return 0;
+}
+
 int main(int argc, char** argv) {
     FILE* f = fopen(filename, "a+");
     
     int mode = define_command(&argc, argv);
-    printf("mode %d\n", mode);
 
     if(f == NULL) {
         fprintf(stderr, "Error opening file %s\n", filename);
         return -1;
+    }
+
+    switch(mode) {
+        case INSERT:
+            insert_todo(f, argv+1);
+            break;
+
+        case COMPLETE:
+            break;
+
+        default:
+            break;
     }
 
     if(fclose(f) == EOF) {
